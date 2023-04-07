20e552c646ed687f9ec7740502ba0036d0dda6cb
Matt Savoy <msavoy@mindgrub.com>
2023-04-06 18:42:35 -0600

    Added Email Signature Templates

---
+++ templates/email-sig-v2.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Template Name: Email Sig V2
+*/
+?>
+<html>
+<body>
+  
+<table class="table__StyledTable-sc-1avdl6r-0 jWJRxL" style="width: 420px; vertical-align: -webkit-baseline-middle; font-size: large; font-family: Arial;" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td>
+<table class="table__StyledTable-sc-1avdl6r-0 jWJRxL" style="width: 100%; vertical-align: -webkit-baseline-middle; font-size: large; font-family: Arial;" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td>
+<h2 class="name__NameContainer-sc-1m457h3-0 hkyYrA" style="margin: 0px; font-size: 20px; color: #67664d; font-weight: 600;">Joseph J. Bowar</h2>
+<p class="job-title__Container-sc-1hmtp73-0 iJcqpv" style="margin: 0px; color: #67664d; font-size: 16px; line-height: 24px;">Managing Principal</p>
+<p class="company-details__CompanyContainer-sc-j5pyy8-0 bEBXsp" style="margin: 0px; font-weight: 500; color: #67664d; font-size: 16px; line-height: 24px;">Capital Markets | Viridian Partners</p>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td height="10"></td>
+</tr>
+<tr><!-- start-->
+<td>
+<table class="table__StyledTable-sc-1avdl6r-0 jWJRxL" style="width: 100%; vertical-align: -webkit-baseline-middle; font-size: large; font-family: Arial;" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="vertical-align: top;"><img class="image__StyledImage-sc-hupvqm-0 fQKUvi" style="display: inline; text-align: left; max-width: 130px;" role="presentation" src="https://i.ibb.co/6Rng3n9/Viridian-Logo-Green.png" width="130" /></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<!-- end -->
+<tr>
+<td height="10"></td>
+</tr>
+<tr>
+<td>
+<table class="table__StyledTable-sc-1avdl6r-0 jWJRxL" style="vertical-align: -webkit-baseline-middle; font-size: large; font-family: Arial;" cellspacing="0" cellpadding="0">
+<tbody>
+<tr style="vertical-align: middle;">
+<td style="vertical-align: middle;" width="10">
+<table class="table__StyledTable-sc-1avdl6r-0 jWJRxL" style="vertical-align: -webkit-baseline-middle; font-size: large; font-family: Arial;" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="vertical-align: bottom;"><span class="contact-info__IconWrapper-sc-mmkjr6-1 hBHfIp" style="display: inline-block; background-color: #67664d;"><img class="contact-info__ContactLabelIcon-sc-mmkjr6-0 dGVIJx" style="display: block; background-color: #67664d;" src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/phone-icon-2x.png" alt="mobilePhone" width="13" /></span></td>
+</tr>
+</tbody>
+</table>
+</td>
+<td style="padding: 0px; color: #67664d;"><a class="contact-info__ExternalLink-sc-mmkjr6-2 bibcmr" style="text-decoration: none; color: #67664d; font-size: 12px;" href="tel:720-317-2027">P: 720-317-2027</a> | <a class="contact-info__ExternalLink-sc-mmkjr6-2 bibcmr" style="text-decoration: none; color: #67664d; font-size: 12px;" href="tel:312-952-6439">C: 312-952-6439</a></td>
+</tr>
+<tr style="vertical-align: middle;">
+<td style="vertical-align: middle;" width="30">
+<table class="table__StyledTable-sc-1avdl6r-0 jWJRxL" style="vertical-align: -webkit-baseline-middle; font-size: large; font-family: Arial;" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="vertical-align: bottom;"><span class="contact-info__IconWrapper-sc-mmkjr6-1 hBHfIp" style="display: inline-block; background-color: #67664d;"><img class="contact-info__ContactLabelIcon-sc-mmkjr6-0 dGVIJx" style="display: block; background-color: #67664d;" src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/email-icon-2x.png" alt="emailAddress" width="13" /></span></td>
+</tr>
+</tbody>
+</table>
+</td>
+<td style="padding: 0px;"><a class="contact-info__ExternalLink-sc-mmkjr6-2 bibcmr" style="text-decoration: none; color: #67664d; font-size: 12px;" href="mailto:jbowar@viridianpartners.com">jbowar@viridianpartners.com</a></td>
+</tr>
+<tr style="vertical-align: middle;">
+<td style="vertical-align: middle;" width="30">
+<table class="table__StyledTable-sc-1avdl6r-0 jWJRxL" style="vertical-align: -webkit-baseline-middle; font-size: large; font-family: Arial;" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="vertical-align: bottom;"><span class="contact-info__IconWrapper-sc-mmkjr6-1 hBHfIp" style="display: inline-block; background-color: #67664d;"><img class="contact-info__ContactLabelIcon-sc-mmkjr6-0 dGVIJx" style="display: block; background-color: #67664d;" src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/link-icon-2x.png" alt="website" width="13" /></span></td>
+</tr>
+</tbody>
+</table>
+</td>
+<td style="padding: 0px;"><a class="contact-info__ExternalLink-sc-mmkjr6-2 bibcmr" style="text-decoration: none; color: #67664d; font-size: 12px;" href="//www.viridianpartners.com">www.viridianpartners.com</a></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+
+
+</body>
+</html>
